aac598d0b613a56d2b84ca4c7dd37270599de4cb
ThaoVy <thaovyluuthanh@gmail.com>
2024-10-06 15:37:37 +0700

    create home and admin pages

---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,95 @@
+import "react-pro-sidebar/dist/css/styles.css";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+
+import { FaGem, FaHeart } from "react-icons/fa";
+import { MdDashboard } from "react-icons/md";
+import sidebarBg from "../../assets/quiz-sidebar.jpg";
+
+export default function SideBar(props) {
+  const { collapsed, toggled, handleToggleSidebar } = props;
+  return (
+    <>
+      <ProSidebar
+        image={sidebarBg}
+        collapsed={collapsed}
+        toggled={toggled}
+        breakPoint="md"
+        onToggle={handleToggleSidebar}
+      >
+        <SidebarHeader>
+          <div
+            style={{
+              padding: "24px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              fontSize: 14,
+              letterSpacing: "1px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            <FaHeart size={"3em"} color={"#00bfff"} />
+            <span style={{ marginLeft: "10px" }}>Quiz Admin</span>
+          </div>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <Menu iconShape="circle">
+            <MenuItem
+              icon={<MdDashboard />}
+              suffix={<span className="badge red">New</span>}
+            >
+              Dashboard
+            </MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu
+              suffix={<span className="badge yellow">3</span>}
+              icon={<FaGem />}
+              title="Features"
+            >
+              <MenuItem>Quản lý Users</MenuItem>
+              <MenuItem>Quản lý Bài Quiz</MenuItem>
+              <MenuItem>Quản lý Câu Hỏi</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter style={{ textAlign: "center" }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{
+              padding: "20px 24px",
+            }}
+          >
+            <a
+              href="https://github.com/azouaoui-med/react-pro-sidebar"
+              target="_blank"
+              className="sidebar-btn"
+              rel="noopener noreferrer"
+            >
+              <span
+                style={{
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                viewSource
+              </span>
+            </a>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
+  );
+}
